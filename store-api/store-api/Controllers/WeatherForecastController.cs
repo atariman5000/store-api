51214d7a39b7b682fa18d0ed6ba1398fd2bec09c
Thomas Morris <Thomas.Morris@plateauinc.com>
2021-09-15 15:08:01 -0500

    Got docker working

--- store-api/store-api/Controllers/WeatherForecastController.cs
+++ store-api/store-api/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace store_api.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly 1", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
