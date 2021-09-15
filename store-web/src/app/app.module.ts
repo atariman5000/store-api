9e6657a2dfd54b135e5f21ec6a5e81f28f45bf7d
Thomas Morris <Thomas.Morris@plateauinc.com>
2021-09-15 12:45:56 -0500

    Working on docker solution

--- store-web/src/app/app.module.ts
+++ store-web/src/app/app.module.ts
@@ -5,17 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EntityDataModule } from '@ngrx/data';
 import { entityConfig } from './entity-metadata';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    EntityDataModule.forRoot(entityConfig)
+    HttpClientModule,
+    /* NgRx Specific Modules */
+    StoreModule.forRoot({}),
+    EffectsModule.forRoot(),
+    EntityDataModule.forRoot(entityConfig),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
