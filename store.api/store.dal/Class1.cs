920565fe0797895029d5a13dbc9da0a180e69cdc
Thomas Morris <Thomas.Morris@plateauinc.com>
2021-09-17 16:57:58 -0500

    Got multi project build working

--- store.api/store.dal/Class1.cs
+++ store.api/store.dal/Class1.cs
@@ -4,5 +4,8 @@ namespace store.dal
 {
     public class Class1
     {
+        public int MyProperty { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
     }
 }
