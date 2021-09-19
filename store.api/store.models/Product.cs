982f466df50b6697e92eae4ce9b8af30aaf219e6
Thomas Morris <Thomas.Morris@plateauinc.com>
2021-09-18 22:23:02 -0500

    working on ef migration

--- store.api/store.models/Product.cs
+++ store.api/store.models/Product.cs
@@ -6,6 +6,6 @@ namespace Store.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public float Price { get; set; }
+        public decimal Price { get; set; }
     }
 }
